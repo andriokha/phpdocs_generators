cd5a47e77959914fa777d369b13184359901fe0b
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-11-11 11:38:03 +0100

    Multibranch phpdoc.

--- init.sh
+++ init.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Bring the different generators.
+rm doctum/*.phar phpdox/*.phar phpdoc/*.phar
 wget -P doctum https://doctum.long-term.support/releases/latest/doctum.phar
 wget -P phpdox http://phpdox.de/releases/phpdox.phar
 wget -P phpdoc https://phpdoc.org/phpDocumentor.phar
 
 # Bring the different repos.
-rm -rf repos/*
+rm -rf repos/drupal repos/gin repos/webform
 cd repos
 git clone https://git.drupalcode.org/project/drupal.git
 git clone https://git.drupalcode.org/project/webform.git
 git clone https://git.drupalcode.org/project/gin.git
+
+rm -rf drupal-branches
+mkdir drupal-branches
+cd drupal-branches
+git clone  --branch '7.x' https://git.drupalcode.org/project/drupal.git 7.x
+git clone  --branch '9.3.x' https://git.drupalcode.org/project/drupal.git 9.3.x
