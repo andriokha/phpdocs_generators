8a28f3f672a39ae4fde46092981c87821c4a810f
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-11-15 09:41:08 +0100

    Bring latest doctum.

--- scripts/open-docs.sh
+++ scripts/open-docs.sh
@@ -4,9 +4,9 @@ open doctum/build/drupal/9.3.x/index.html
 open doctum/build/webform/6.x/index.html
 # open doctum/build/gin/8.x-3.x/index.html
 
-open phpdoc/build/drupal7/index.html
-open phpdoc/build/drupal9/index.html
-open phpdoc/build/webform/index.html
+# open phpdoc/build/drupal7/index.html
+# open phpdoc/build/drupal9/index.html
+# open phpdoc/build/webform/index.html
 # open phpdoc/build/gin/index.html
 
 # open phpdox/build/drupal/html/index.xhtml
